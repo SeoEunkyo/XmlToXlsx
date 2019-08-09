f995458a01422edcc77b02e8ebe67628ce13a84b
서은교/(협력사) 대리/검사기술팀(eunkyo.seo@lgepartner.com) <eunkyo.seo@lge.com>
2019-08-09 16:18:26 +0900

    initail

--- api/xmltoxlsx/xmltoxlsx.ctrl.js
+++ api/xmltoxlsx/xmltoxlsx.ctrl.js
@@ -1,16 +1,56 @@
+const XLSX = require('xlsx')
+const fs = require('fs')
+var xml2json = require('xml2json');
+
+
 const index = (req,res) => { 
     //console.log(req.query.limit)
     //if(req.query.limit === 2)
     //{
     //    res.json(users[2])
     //}
-    res.render('xmltoxlsx');
+    res.render('xmltoxlsx')
     
 }
 
 const convert = (req, res) =>{
 
-    console.log(req.body.content) 
+    
+    const XMLData = req.body.content
+    var jsonformat = xml2json.toJson(XMLData)
+    const obj = JSON.parse(jsonformat)
+    jsonformat = obj.DocumentElement.Table
+
+    console.log(jsonformat) 
+    
+
+    var wb = XLSX.utils.book_new();
+    var ws = XLSX.utils.json_to_sheet(jsonformat )
+    XLSX.utils.book_append_sheet(wb, ws, 'Converted Sheet' );
+
+    XLSX.writeFile(wb, "./public/sheetTemp.xlsx");
+
+    const filePath =  "./public/sheetTemp.xlsx"
+    var fileName = 'Result'
+    
+    
+    fs.exists(filePath, function(exists){
+        console.log(filePath)
+        if (exists) {     
+          // Content-type is very interesting part that guarantee that
+          // Web browser will handle response in an appropriate manner.
+          res.writeHead(200, {
+            "Content-Type": "application/octet-stream",
+            "Content-Disposition": "attachment; filename=" + fileName
+          });
+          fs.createReadStream(filePath).pipe(res);
+        } else {
+          res.writeHead(400, {"Content-Type": "text/plain"});
+          res.end("ERROR File does not exist");
+        }
+    })
+
+
 
 
 
